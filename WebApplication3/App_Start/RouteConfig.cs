599ec11f50f28658e2e0451764caab7349c1e76e
Vichian <chinatip.v@ku.th>
2018-08-22 10:43:04 +0700

    Change routes

--- WebApplication3/App_Start/RouteConfig.cs
+++ WebApplication3/App_Start/RouteConfig.cs
@@ -14,6 +14,24 @@ namespace WebApplication3
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "search",
+                url: "path/search/{action}",
+                defaults: new { controller = "Search", action = "Result", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "search-details",
+                url: "word/{id}",
+                defaults: new { controller = "Search", action = "Details", id =  UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "home",
+                url: "path/home/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
